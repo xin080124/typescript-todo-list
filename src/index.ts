d33fec1336a9cfc5b31f1da4744c604054962695
NancyZhang <Nancy.Zhang@fisherpaykel.com>
2023-02-21 14:47:01 +1300

    add react pkgs

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,13 @@
-import { v4 as uuidV4 } from "uuid"
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-type Task = {
-  id: string
-  title: string
-  completed: boolean
-  createdAt: Date
-}
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 
-const list = document.querySelector<HTMLUListElement>("#list")
-const form = document.getElementById("new-task-form") as HTMLFormElement | null
-const input = document.querySelector<HTMLInputElement>("#new-task-title")
-const tasks: Task[] = loadTasks()
-tasks.forEach(addListItem)
-
-form?.addEventListener("submit", e => {
-  e.preventDefault()
-
-  if (input?.value == "" || input?.value == null) return
-
-  const newTask: Task = {
-    id: uuidV4(),
-    title: input.value,
-    completed: false,
-    createdAt: new Date(),
-  }
-  tasks.push(newTask)
-  saveTasks()
-
-  addListItem(newTask)
-  input.value = ""
-})
-
-function addListItem(task: Task) {
-  const item = document.createElement("li")
-  const label = document.createElement("label")
-  const checkbox = document.createElement("input")
-  checkbox.addEventListener("change", () => {
-    task.completed = checkbox.checked
-    saveTasks()
-  })
-  checkbox.type = "checkbox"
-  checkbox.checked = task.completed
-  label.append(checkbox, task.title)
-  item.append(label)
-  list?.append(item)
-}
-
-function saveTasks() {
-  localStorage.setItem("TASKS", JSON.stringify(tasks))
-}
-
-function loadTasks(): Task[] {
-  const taskJSON = localStorage.getItem("TASKS")
-  if (taskJSON == null) return []
-  return JSON.parse(taskJSON)
-}
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
